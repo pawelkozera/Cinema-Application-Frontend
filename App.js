2cb910b889e10e94160cdd1e533ffaf8ac9650d3
Pawel Kozera <pawelkozera1@gmail.com>
2024-03-14 12:16:54 +0100

    Testing fetching data from backend

--- App.js
+++ App.js
@@ -1,10 +1,25 @@
+import React, { useEffect, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [users, setUsers] = useState([]);
+
+
+  useEffect(() => {
+    fetch("http://localhost:8080/api/test")
+      .then(response => response.json())
+      .then(data => setUsers(data))
+      .catch(error => console.error('Error fetching users:', error));
+  }, []);
+
+  useEffect( () => {
+    console.log(users);
+  }, [users]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{users}</Text>
       <StatusBar style="auto" />
     </View>
   );
